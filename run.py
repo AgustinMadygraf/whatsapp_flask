2f670c76826ed68d0e692323238057663604400d
AgustinMadygraf <agustin.mtto.madygraf>
2025-04-29 06:37:54 -0300

    Remove deprecated files and restructure the project for improved organization

---
+++ run.py
@@ -0,0 +1,22 @@
+"""
+Path: run.py
+"""
+
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
+  content_variables='{"1":"12/1","2":"3pm"}',
+  to='whatsapp:+5491135162685'
+)
+
+print(message.sid)
